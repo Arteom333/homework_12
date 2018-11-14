89ede513dffbd1ec366543c329a5482c31db914f
Arteom333 <porchiesku@bk.ru>
2018-11-14 23:00:32 +0200

    APT

---
+++ script12.js
@@ -0,0 +1,9 @@
+// PROMPT
+var promptResult = prompt('x');
+console.log('promptResult ', promptResult);
+document.getElementById('prompt-result').innerText = promptResult;
+
+var promptResult2 = prompt('n');
+console.log('prompt-result2', promptResult2);
+document.getElementById('prompt-result2').innerText = promptResult2
+
