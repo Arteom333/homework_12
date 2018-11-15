8531491e066db3bbbe982419b1f1555204529d29
Arteom333 <43860271+Arteom333@users.noreply.github.com>
2018-11-15 14:46:00 +0200

    Update script12.js

--- script12.js
+++ script12.js
@@ -1,9 +1,9 @@
 // PROMPT
-var promptResult = prompt("Введите x", '');
+var promptResult = prompt("Введите x", 'x =');
 console.log('promptResult ', promptResult);
 document.getElementById('prompt-result').innerText = promptResult;
 
-var promptResult2 = prompt("Введите n", '');
+var promptResult2 = prompt("Введите n", 'n =');
 console.log('prompt-result2', promptResult2);
 document.getElementById('prompt-result2').innerText = promptResult2
 
